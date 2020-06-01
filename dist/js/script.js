caeb40084b70c61e513331862d4729dfb5277f7b
nadya <nadya.wyn@ya.ru>
2020-06-01 15:04:14 +0500

    Added animations on scroll

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,13 +7,54 @@ $(document).ready(function () {
 	//more Animation
 
 });
-/*
+
 $(window).scroll(function () {
-	$('.mov').each(function () {
+	$('.header__onion').each(function () {
 		const imagePos = $(this).offset().top;
 		const topOfWindow = $(window).scrollTop();
 		if (imagePos < topOfWindow + 200) {
-			$(this).addClass('animated__bounceInRight');
+			$(this).addClass('animated_l');
 		}
 	});
-}); */
+	$('.discover__leaf').each(function () {
+		const imagePos = $(this).offset().top;
+		const topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow + 200) {
+			$(this).addClass('animated_def');
+		}
+	});
+	$('.tap__mushroom').each(function () {
+		const imagePos = $(this).offset().top;
+		const topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow + 200) {
+			$(this).addClass('animated_l');
+		}
+	});
+	$('.track__foods').each(function () {
+		const imagePos = $(this).offset().top;
+		const topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow + 200) {
+			$(this).addClass('animated_b');
+		}
+	});
+
+	$('.get-app__leaves').each(function () {
+		const imagePos = $(this).offset().top;
+		const topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow + 200) {
+			$(this).addClass('animated_def');
+		}
+	});
+
+	$('.get-app__foods').each(function () {
+		const imagePos = $(this).offset().top;
+		const topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow + 200) {
+			$(this).addClass('animated_b');
+		}
+	});
+
+});
+
+
+
