7aa8f8a2b81134a5cb23c123ed858c0e9adc659a
nadya <nadya.wyn@ya.ru>
2020-06-01 10:39:17 +0500

    Added custom animation

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,17 @@ $(document).ready(function () {
 	//Animation 
 	new WOW().init();
 
-});  
+
+	//more Animation
+
+});
+
+$(window).scroll(function () {
+	$('.mov').each(function () {
+		const imagePos = $(this).offset().top;
+		const topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow + 200) {
+			$(this).addClass('animate__shakeX');
+		}
+	});
+});
